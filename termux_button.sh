503f4ba9e8668fb211126524cc356c7f6ce290a1
A-Gvmt <user@localhost.localdomain>
2020-05-23 12:25:09 +0700

    Perbaikan Bug

--- termux_button.sh
+++ termux_button.sh
@@ -8,29 +8,30 @@ termux-setup-storage && clear
 printf "Menginstall Program Program yang Sering digunakan\n\n"
 # Hanya Agar Tampilan Saat Installasi Terlihat Rapih
 apt update && apt upgrade -y && clear
-apt install python3 -y && clear
-apt install python2 -y && clear
-apt install php -y && clear
-apt install ruby -y && clear
-apt install figlet -y && clear
-apt install nano -y && clear
+[[ -z $(command -v python3) ]] && apt install python3 -y && clear && py3=1
+[[ -z $(command -v python2) ]] && apt install python2 -y && clear && py2=1
+[[ -z $(command -v php) ]] && apt install php -y && clear
+[[ -z $(command -v ruby) ]] && apt install ruby -y && clear && rby=1
+[[ -z $(command -v figlet) ]] && apt install figlet -y && clear
+[[ -z $(command -v nano) ]] && apt install nano -y && clear
 
-python3 -m pip install mechanize bs4 requests && clear
-python2 -m pip install mechanize bs4 requests && clear
+[[ $py3 == 1 ]] && python3 -m pip install mechanize bs4 requests && clear
+[[ $py2 == 1 ]] && python2 -m pip install mechanize bs4 requests && clear
 
-gem install lolcat && clear
+[[ $rby == 1 ]] && gem install lolcat && clear
 
 clear
 printf "\nMenambahkan Bar Tombol\n" 
 [[ ! -d ~/.termux ]] && mkdir ~/.termux
 
 if [[ ! -f ~/.termux/termux.properties ]]; then
-    echo "echo "extra-keys = [ \
+    echo "extra-keys = [ \
     ['ESC','|','/','HOME','UP','END','PGUP','DEL'], \
     ['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN','BKSP'] \
-    ]" > termux.properties && termux-reload-settings" > ~/.termux/termux.properties && termux-reload-settings 
+    ]" > ~/.termux/termux.properties && termux-reload-settings 
 else
     printf "Tombol gagal Terinstall Karena File Sudah Ada\n"
 fi
 
-printf "\nSilahkan Mulai Ulang Termux Anda\n"
+printf "\nSilahkan Masuk lagi Setelah ini"
+exit
